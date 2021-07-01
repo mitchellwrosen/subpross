e93fc1d8979e9d466442444f6b68c60049ebd24b
Mitchell Rosen <mitchell@simspace.com>
2021-07-01 00:05:56 -0400

    linewise

--- src/Subpross.hs
+++ src/Subpross.hs
@@ -1,22 +1,32 @@
+{-# LANGUAGE UnboxedTuples #-}
+
 module Subpross
   ( withProcess,
-    Source,
+    Config (..),
     Event (..),
+    Source (..),
+    -- debugging
+    onLines,
+    linewise,
+    listSource,
   )
 where
 
 import Control.Concurrent.STM.TQueue
 import Control.Exception (IOException, bracket, finally, try)
+import Control.Monad (when)
 import Control.Monad.STM (STM, atomically)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import Data.Coerce (coerce)
 import Data.Function
 import Data.Functor (void)
-import Data.IORef (newIORef, readIORef, writeIORef)
+import Data.IORef
 import Data.Text (Text)
 import qualified Data.Text as Text
-import Data.Void (Void)
+import qualified Data.Text.Encoding as Text
+import qualified Data.Text.Internal.Private as Text (span_)
+import qualified Data.Text.Unsafe as Text (unsafeTail)
 import GHC.Generics (Generic)
 import GHC.TypeLits (Symbol)
 import qualified Ki
@@ -29,9 +39,6 @@ import qualified System.Process as Process
 import qualified System.Process.Internals as Process
 import Prelude hiding (read)
 
-type Source a =
-  (Maybe a -> IO ()) -> (Maybe Void -> IO ())
-
 -- TODO cwd, env
 data Config = Config
   { name :: Text,
@@ -43,7 +50,80 @@ data Event a
   = Stdout a
   | Stderr a
   | Exit ExitCode
-  deriving stock (Eq, Generic, Show)
+  deriving stock (Eq, Functor, Generic, Show)
+
+newtype Source a = Source
+  {unSource :: (Maybe a -> IO ()) -> IO ()}
+  deriving stock (Functor)
+
+instance Monoid (Source a) where
+  mappend = (<>)
+  mempty = Source mempty
+
+instance Semigroup (Source a) where
+  Source xs <> Source ys =
+    Source \k -> do
+      xs k
+      ys k
+
+listSource :: [a] -> Source a
+listSource xs0 =
+  Source \k ->
+    (`fix` xs0) \again -> \case
+      [] -> k Nothing
+      x : xs -> do
+        k (Just x)
+        again xs
+
+linewise :: Source (Event ByteString) -> Source (Event Text)
+linewise source =
+  Source \k -> do
+    outRef <- newIORef (S Text.empty Text.streamDecodeUtf8)
+    errRef <- newIORef (S Text.empty Text.streamDecodeUtf8)
+    drain source \case
+      Stdout chunk -> alterIORef outRef \s -> feed (k . Just . Stdout) s chunk
+      Stderr chunk -> alterIORef errRef \s -> feed (k . Just . Stderr) s chunk
+      Exit code -> do
+        S out _ <- readIORef outRef
+        when (not (Text.null out)) (k (Just (Stdout out)))
+        S err _ <- readIORef errRef
+        when (not (Text.null err)) (k (Just (Stderr out)))
+        k (Just (Exit code))
+
+feed :: (Text -> IO ()) -> S -> ByteString -> IO S
+feed k (S text0 decode0) chunk =
+  case decode0 chunk of
+    Text.Some text1 _ decode1 -> do
+      text2 <- onLines k (text0 <> text1)
+      pure (S text2 decode1)
+
+data S
+  = S !Text !(ByteString -> Text.Decoding)
+
+-- | Perform an action on each line in the input, and return the (possibly empty) end of the input that did not end in a
+-- newline.
+--
+--      > onLines putStrLn "foo\nbar\n"
+--      foo  -- side-effect
+--      bar  -- side-effect
+--      ""   -- return value
+--
+--     > onLines putStrLn  "foo\nbar"
+--     foo   -- side-effect
+--     "bar" -- return-value
+onLines :: (Text -> IO ()) -> Text -> IO Text
+onLines action =
+  fix \again xs ->
+    case Text.null xs of
+      False ->
+        case Text.span_ (/= '\n') xs of
+          (# ys, zs #) ->
+            case Text.null zs of
+              False -> do
+                action ys
+                again (Text.unsafeTail zs)
+              True -> pure ys
+      True -> pure xs
 
 withProcess :: Config -> Source ByteString -> (Source (Event ByteString) -> IO r) -> IO r
 withProcess Config {arguments, foreground, name} stdin k =
@@ -79,14 +159,15 @@ withProcess Config {arguments, foreground, name} stdin k =
             (readEvent, writeEvent) <- newQueue
             Ki.scoped \scope -> do
               Ki.fork_ scope (handleProcess stdinW stdoutR stderrR process stdin (atomically . writeEvent))
-              k \callback _ ->
-                fix \again -> do
-                  event <- atomically readEvent
-                  callback (Just event)
-                  case event of
-                    Stdout _ -> again
-                    Stderr _ -> again
-                    Exit _ -> callback Nothing
+              k do
+                Source \callback ->
+                  fix \again -> do
+                    event <- atomically readEvent
+                    callback (Just event)
+                    case event of
+                      Stdout _ -> again
+                      Stderr _ -> again
+                      Exit _ -> callback Nothing
 
 terminateProcess :: Process.ProcessHandle -> IO ()
 terminateProcess process = do
@@ -136,15 +217,21 @@ handleProcess stdinW stdoutR stderrR process stdin handleEvent =
             action chunk
             again
 
--- | Drain a 'Source' by executing a callback on each item.
+-- | Drain a 'Source' with an action to run on each item.
 drain :: Source a -> (a -> IO ()) -> IO ()
-drain source callback =
-  source (maybe (pure ()) callback) Nothing
+drain source action =
+  unSource source (maybe (pure ()) action)
 
 newQueue :: IO (STM a, a -> STM ())
 newQueue = do
   queue <- newTQueueIO
   pure (readTQueue queue, writeTQueue queue)
+
+alterIORef :: IORef a -> (a -> IO a) -> IO ()
+alterIORef ref action = do
+  x0 <- readIORef ref
+  x1 <- action x0
+  writeIORef ref x1
 
 ------------------------------------------------------------------------------------------------------------------------
 -- Handle utils
